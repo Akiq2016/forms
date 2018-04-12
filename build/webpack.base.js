fa90bb1936d62375bd463729d35d8a44c3d62b6a
akiq2016 <akiq2016@gmail.com>
2018-04-12 13:42:09 +0800

    init

---
+++ build/webpack.base.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+
+module.exports = {
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, use: "ts-loader" },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: { importLoaders: 2, minimize: true, sourceMap: true },
+            },
+            {
+              loader: 'postcss-loader',
+              options: { sourceMap: true },
+            },
+            {
+              loader: 'sass-loader',
+              options: { sourceMap: true },
+            },
+          ]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: path.join(__dirname, "src/index.html") }),
+    new ExtractTextPlugin('style.css'),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 5555,
+    hot: true
+  },
+  devtool: 'eval-source-map',
+}

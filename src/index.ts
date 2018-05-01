0098a3eaeebb4aca1962478d10da9d59e130da2e
akiq2016 <akiq2016@gmail.com>
2018-05-01 22:56:39 +0800

    wip: custom drag element

--- src/index.ts
+++ src/index.ts
@@ -7,41 +7,61 @@ customElements.define('draggable-item', DraggableItem)
 
 class DraggableWrapper extends HTMLElement {
 
-  activedItemId: number
+  draggingItem: DraggableItem
+  oldPositionIndex: number
 
   constructor() {
     super()
 
-    // Setup a drag relate listeners on <draggable-wrapper> itself.
+    // Setup a drag related listeners on <draggable-wrapper> itself.
     this.addEventListener('dragstart', ({ target }): void => {
-      if (target instanceof DraggableItem) {
-        this.activedItemId = +target.dataset.dragId
-      }
+      if (!(target instanceof DraggableItem)) return
+
+      this.draggingItem = target
+      this.oldPositionIndex = this.getItemIndexInList(target)
     }, false)
 
     this.addEventListener('dragenter', ({ target }): void => {
-      if (target instanceof DraggableItem) {
-        const newDragId = +target.dataset.dragId
+      if (!(target instanceof DraggableItem)) return
 
-        // As a draggable item might have several blocks inside.
-        // when it go in-out the blocks inside,
-        // the draggable item's dragenter event is invoked.
-        // So check id, if is the same id, it still in itself.
-        if (newDragId !== this.activedItemId) {
-          console.log(target, newDragId, this.activedItemId)
-          console.log()
-        }
+      // As a draggable item might have several blocks inside.
+      // when it go in-out these blocks,
+      // the draggable item's related events invoked.
+      // So if is the same id, it still in itself.
+      const newDragIndex = this.getItemIndexInList(target)
+      if (newDragIndex === this.oldPositionIndex) return
+
+      // todo
+      if (newDragIndex < this.oldPositionIndex) {
+        this.insertBefore(this.draggingItem, target)
+        this.oldPositionIndex = newDragIndex
       }
+
     }, false)
 
-    this.addEventListener('dragleave', (e): void => {
+    this.addEventListener('dragleave', ({ target }): void => {
+      if (!(target instanceof DraggableItem)) return
 
+      const newDragIndex = this.getItemIndexInList(target)
+      if (newDragIndex === this.oldPositionIndex) return
+
+      if (newDragIndex > this.oldPositionIndex) {
+        target.nextElementSibling
+          ? this.insertBefore(this.draggingItem, target.nextElementSibling)
+          : this.appendChild(this.draggingItem)
+      }
+      this.oldPositionIndex = newDragIndex
     }, false)
 
     this.addEventListener('dragend', (e): void => {
 
     }, false)
 
+  }
+  
+  getItemIndexInList (item: DraggableItem): number {
+    return Array.from(this.querySelectorAll('draggable-item'))
+      .findIndex(v => v === item)
   }
 }
 customElements.define('draggable-wrapper', DraggableWrapper)

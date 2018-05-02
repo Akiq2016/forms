f0ae0c706dd737ece87262f39cfcb934fee07d91
akiq2016 <akiq2016@gmail.com>
2018-05-02 13:31:20 +0800

    chore: fix css hmr

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -3,7 +3,43 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require("extract-text-webpack-plugin")
 
+const isDevMode = (() => {
+  return (process.env.NODE_ENV.trim() === 'development')
+})()
+
+function getScssLoader () {
+  const mode = isDevMode ? 'development' : 'production'
+
+  if (mode === 'development') {
+    return [
+      { loader: "style-loader" },
+      { loader: "css-loader", options: { importLoaders: 2 } },
+      { loader: 'postcss-loader' },
+      { loader: "sass-loader" }
+    ]
+  } else {
+    return ExtractTextPlugin.extract({
+      fallback: 'style-loader',
+      use: [
+        {
+          loader: 'css-loader',
+          options: { importLoaders: 2, minimize: true, sourceMap: true },
+        },
+        {
+          loader: 'postcss-loader',
+          options: { sourceMap: true },
+        },
+        {
+          loader: 'sass-loader',
+          options: { sourceMap: true },
+        },
+      ]
+    })
+  }
+}
+
 module.exports = {
+  mode: isDevMode ? 'development' : 'production',
   resolve: {
     extensions: [".ts", ".js"]
   },
@@ -12,23 +48,7 @@ module.exports = {
       { test: /\.tsx?$/, use: "ts-loader" },
       {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-              options: { importLoaders: 2, minimize: true, sourceMap: true },
-            },
-            {
-              loader: 'postcss-loader',
-              options: { sourceMap: true },
-            },
-            {
-              loader: 'sass-loader',
-              options: { sourceMap: true },
-            },
-          ]
-        })
+        use: getScssLoader()
       }
     ]
   },

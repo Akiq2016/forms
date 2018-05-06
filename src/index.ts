ab8fd08954184c33271919bd36d82940a884289c
akiq2016 <akiq2016@gmail.com>
2018-05-06 18:59:00 +0800

    wip

--- src/index.ts
+++ src/index.ts
@@ -6,52 +6,50 @@ class DraggableItem extends HTMLElement {
 
 class DraggableWrapper extends HTMLElement {
 
+  computedStyle: any
+  boundingClientRect: { left, top, width, height }
+
   draggingItem: DraggableItem
-  draggingCopyItem: DraggableItem
   oldPositionIndex: number
-  MousePositionToCopyItemBorder: { width, height }
+  mousePositionToItemBorder: { width, height }
 
   constructor() {
     super()
 
+    this.computedStyle = getComputedStyle(this)
+    this.boundingClientRect = this.getBoundingClientRect()
+
+    // so there won't have a dragging cursor effect
     this.addEventListener('dragstart', (e): void => {
       e.preventDefault()
     })
 
-    // Setup a drag related listeners on <draggable-wrapper> itself.
+    // Using mouse related events to simulate the dragging effect.
     this.addEventListener('mousedown', ({ target, clientX, clientY }): void => {
       if (!(target instanceof DraggableItem)) return
 
-      let { left, top, width, height } = target.getBoundingClientRect()
+      let { left, top } = target.getBoundingClientRect()
 
-      this.oldPositionIndex = this.getItemIndexInList(target)
-      this.MousePositionToCopyItemBorder = {
-        width: clientX - left,
-        height: clientY - top
-      }
-  
       this.draggingItem = target
-      this.draggingItem.style.cssText = `
-        position: relative;
-        top: 0;
-        left: 0;
-        z-index: 2;
-        transform: translate(${left}px, ${top}px);
-      `
-  
-      console.log('mousedown', target)
+      this.oldPositionIndex = this.getItemIndexInList(target)
+      this.draggingItem.style.cssText = this.generateItemStyle({target})
+      this.mousePositionToItemBorder = { width: clientX - left, height: clientY - top }
     }, false)
 
     this.addEventListener('mouseover', ({ target, clientX, clientY }): void => {
+      // mouseover event is triggered when going through the current target.
+      // for simulating drag, we need to ensure now there has a dragging stuff,
+      // then the mouseover is the same as dragover.
       if (!(target instanceof DraggableItem) || !this.draggingItem) return
 
-      let { width, height } = this.MousePositionToCopyItemBorder
-      this.draggingItem.style.transform = `translate(${clientX - width}px, ${clientY - height}px)`
-      console.log(this.draggingItem.style.transform)
+      let x = clientX - this.mousePositionToItemBorder.width - this.boundingClientRect.left
+      let y = clientY - this.mousePositionToItemBorder.height - this.boundingClientRect.top
+
+      this.draggingItem.style.left = `${x}px`
+      this.draggingItem.style.top = `${y}px`
 
       // As a draggable item might have several blocks inside.
-      // when it go in-out these blocks,
-      // the draggable item's related events invoked.
+      // when it go in-out these blocks, the draggable item's related events invoked.
       // So if is the same id, it still in itself.
       const newDragIndex = this.getItemIndexInList(target)
       if (newDragIndex === this.oldPositionIndex) return
@@ -69,13 +67,28 @@ class DraggableWrapper extends HTMLElement {
     this.addEventListener('mouseup', (e): void => {
       this.draggingItem.style.cssText = ''
       this.draggingItem = null
-      console.log('mouseup', e.target)
     }, false)
   }
 
   getItemIndexInList(item: DraggableItem): number {
     return Array.from(this.querySelectorAll('draggable-item'))
       .findIndex(v => v === item)
+  }
+
+  generateItemStyle({ target }): string {
+    let { left, top } = target.getBoundingClientRect()
+    let { width, height } = getComputedStyle(target)
+    let item = { left, top, width, height }
+    let parent = {
+      left: this.boundingClientRect.left,
+      top: this.boundingClientRect.top
+    }
+
+    return `position: absolute;z-index: 1;
+      left: ${item.left - parent.left}px;
+      top: ${item.top - parent.top}px;
+      width: ${item.width};
+      height: ${item.height};`
   }
 }
 
